5c6fcebfdacf41b2f64e9095029d20ae3c165316
daekyeonghan <hdk1008@gmail.com>
2023-03-01 23:27:10 +0900

    가계부 프로젝트 git clone

---
+++ cashbook/src/main/java/com/cash/CashbookApplication.java
@@ -0,0 +1,13 @@
+package com.cash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashbookApplication.class, args);
+	}
+
+}
